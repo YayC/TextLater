cf673aa60d3913f9fbdf58e528da369c9926ad5b
Apprentice <apprentice@devbootcamp.com>
2013-03-28 16:55:24 -0700

    break across multiple texts working

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,8 +4,21 @@ get '/' do
 end
 
 post '/text/schedule' do
-  p params[:text]
+  texts = 1 + params[:text][:body].length / 160
+  if texts > 3
+    return "You crazy, that's #{texts} texts at once. Pick up the phone!"
+  end
+
+  if texts == 1
+    Text.send(params[:text])
+    redirect '/'
+  else
+    body_chars = params[:text][:body].chars.to_a
+    texts.times do
+      msg = body_chars.shift(160).join("")
+      Text.send( :to => params[:text][:to], :body => msg )
+    end
+    redirect '/'
+  end
   
-  Text.send(params[:text])
-  redirect '/'
 end
